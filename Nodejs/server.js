3ca514b3cb0ac96b58f76d216178126c873467a9
Nehemiah Damian <Nehemiahdamian22@gmail.com>
2025-01-18 22:41:45 +0800

    Intro to HTTP method and common commands (context can be read in the code)

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -4,6 +4,7 @@ const server = http.createServer((req, res)=>{
   // res.setHeader('Content-Type', 'text/html')
   // res.write("hello");
   // res.end('<h1>Hello world</h1>');
+  //console.log("hello")
   res.writeHead(500, {'Content-type': 'application/json'})
   res.end(JSON.stringify({message:"Server Error"}))
 })
