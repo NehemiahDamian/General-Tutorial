1faa528757b3672d62af728dcdf547b1c041ad63
Nehemiah <Nehemiahdamian22@gmail.com>
2025-01-19 23:32:31 +0800

    Simple HTTPS routing

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import http from "http";
 const PORT = process.env.PORT
 const server = http.createServer((req, res)=>{
-  // res.setHeader('Content-Type', 'text/html')
-  // res.write("hello");
-  // res.end('<h1>Hello world</h1>');
-  //console.log("hello")
-  res.writeHead(500, {'Content-type': 'application/json'})
-  res.end(JSON.stringify({message:"Servedasdar Error"}))
+  if(req.url === "/"){
+    res.writeHead(200, {"Content-Type": "text/html"} )
+    res.end("<h1>home</h1>")
+  }
+  else if(req.url === "/about"){
+    res.writeHead(200, {"content-Type": "text/html"})
+    res.end("<h1>About</h1>")
+
+  }
+  else{
+    res.writeHead(404, {"Content-Type": "text/html"})
+    res.end("<h1>not found</h1>")
+  }
+ 
+  // res.writeHead(500, {'Content-type': 'application/json'})
+  // res.end(JSON.stringify({message:"Servedasdar Error"}))
 })
 
 server.listen(PORT,()=>{
