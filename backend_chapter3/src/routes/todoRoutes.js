c419688a46ab3a7157734f74ee4da4d7ac8ce25a
Nehemiah Damian <Nehemiahdamian22@gmail.com>
2025-01-21 00:25:36 +0800

    Creating databases and configuring routers

---
+++ backend_chapter3/src/routes/todoRoutes.js
@@ -0,0 +1,24 @@
+import express from "express"
+import db from "../db.js"
+
+const router = express.Router();
+
+// naka log in na users
+router.get("/",(req,res)=>{
+
+})
+
+// creating a new todo
+router.post("/",(req, res)=>{
+
+})
+
+router.put("/:id",(req, res)=>{
+
+})
+
+router.delete("/:id",(req,res)=>{
+
+})
+
+export default router;
