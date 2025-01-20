01ba55db3bf04a76d01c44e6c244c3330ffedcc8
Nehemiah Damian <Nehemiahdamian22@gmail.com>
2025-01-21 00:25:36 +0800

    Creating databases and configuring routers

---
+++ backend_chapter3/src/routes/authRoutes.js
@@ -0,0 +1,17 @@
+import express, { Router } from "express"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import db from "../db.js"
+
+const router = express.Router()
+
+router.post("/register",()=>{
+
+})
+
+router.post("login",()=>{
+
+})
+
+
+export default router;
--- backend_chapter3/src/server.js
+++ backend_chapter3/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import path, {dirname} from 'path'
 import { fileURLToPath } from 'url'
+import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -20,7 +22,9 @@ app.get('/',(req, res)=>{
   res.sendFile(path.join(__dirname, "public", "index.html"))
 })
 
-
+//Routes
+app.use('/auth', authRoutes)
+app.use('/todos', todoRoutes)
 
 app.listen(PORT,(req, res)=>{
   console.log(`${PORT} Running`)
